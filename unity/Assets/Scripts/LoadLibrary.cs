bc89124a8d081b62128dc997e1139ee93d97de18
florianvazelle <ponythugflorian@gmail.com>
2020-11-14 10:59:28 +0100

    Add example of BaseModel c++

--- unity/Assets/Scripts/LoadLibrary.cs
+++ unity/Assets/Scripts/LoadLibrary.cs
@@ -6,26 +6,10 @@ namespace TestCSharpLibrary
 
     public static class LoadLibrary
     {
-
-        [DllImport("example")]
-        public static extern int GetRandom();
-
-        [DllImport("example")]
-        public static extern int pre_alloc_test(IntPtr pArrayOfDouble);
-
-        [DllImport("example")]
-        public static extern int alloc_in_test(IntPtr pArrayOfDouble);
-
-        [DllImport("example")]
-        public static extern void my_free(IntPtr pArrayOfDouble);
-
-        [DllImport("example")]
-        public static extern void write();
-
         [DllImport("example")]
         public static extern IntPtr create_linear_model(int inputs_count);
 
         [DllImport("example")]
-       public static extern void delete_linear_model(IntPtr model);
+        public static extern void delete_linear_model(IntPtr model);
     }
 }
