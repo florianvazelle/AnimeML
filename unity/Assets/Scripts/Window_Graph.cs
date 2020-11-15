c1f88d56c5d9cc1ffd016a4f13d674e6aa28eb34
Shen <s_lin97@hotmail.com>
2020-11-15 17:25:00 +0100

    AddAxis

--- unity/Assets/Scripts/Window_Graph.cs
+++ unity/Assets/Scripts/Window_Graph.cs
@@ -8,15 +8,19 @@ public class Window_Graph : MonoBehaviour {
 
     [SerializeField] private Sprite circleSprite;
     private RectTransform graphContainer;
+    private RectTransform labelTemplateX;
+    private RectTransform labelTemplateY;
 
     private void Awake() {
         graphContainer = transform.Find("graphContainer").GetComponent<RectTransform>();
+        labelTemplateX = graphContainer.Find("labelTemplateX").GetComponent<RectTransform>();
+        labelTemplateY = graphContainer.Find("labelTemplateY").GetComponent<RectTransform>();
 
         //Show one Circle
-        CreateCircle(new Vector2(10, 10));
+        //CreateCircle(new Vector2(10, 10));
         
         //Show the entire Circle
-        List<int> valueList = new List<int>() { 5, 98, 56, 45, 30, 22, 17, 15, 13, 17, 25, 37, 40, 36, 33 };
+        List<int> valueList = new List<int>() { 5, 25, 37, 40, 65, 88 };
         ShowGraph(valueList);
     }
 
@@ -41,7 +45,26 @@ public class Window_Graph : MonoBehaviour {
             float xPosition = xSize + i * xSize;
             float yPosition = (valueList[i] / yMaximum) * graphHeight;
             GameObject circleGameObject = CreateCircle(new Vector2(xPosition, yPosition));
+
+            RectTransform labelX = Instantiate(labelTemplateX);
+            labelX.SetParent(graphContainer, false);
+            labelX.gameObject.SetActive(true);
+            labelX.anchoredPosition = new Vector2(xPosition, -7f);
+            labelX.GetComponent<Text>().text = i.ToString();
         }
+
+        int separatorCount = 10;
+        for (int i = 0; i <= separatorCount; i++)
+        {
+            RectTransform labelY = Instantiate(labelTemplateY);
+            labelY.SetParent(graphContainer, false);
+            labelY.gameObject.SetActive(true);
+            float normalizedValue = i * 1f / separatorCount;
+            labelY.anchoredPosition = new Vector2(-7f, normalizedValue * graphHeight);
+            labelY.GetComponent<Text>().text = Mathf.RoundToInt(normalizedValue * yMaximum).ToString();
+
+        }
+
     }
 
 }
