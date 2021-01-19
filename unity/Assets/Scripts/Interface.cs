43269a24aa8fefbdb03e1042e778e47e90a62021
Antoine <antoine.thenevin@gmail.com>
2021-01-20 00:13:09 +0100

    add file loader

--- unity/Assets/Scripts/Interface.cs
+++ unity/Assets/Scripts/Interface.cs
@@ -8,6 +8,7 @@ using RapidGUI;
 
 public class Interface : MonoBehaviour {
     private Rect windowRect = new Rect(0, 0, 500, 500);
+    //public string picturePath;
     public string picturePath;
     public string modelPath;
     public double result;
@@ -22,9 +23,13 @@ public class Interface : MonoBehaviour {
     private IntPtr model;
 
     public void Start() {
+        picturePath = "Load a picture here";
+        modelPath = "Load a model here";
         model = IntPtr.Zero;
         modelType = ModelType.MLP;
         isClassification = true;
+        epochs = 100;
+        learningRate = 0.15f;
     }
 
 	private void OnGUI() {
@@ -36,10 +41,21 @@ public class Interface : MonoBehaviour {
         // Analyse part
         GUILayout.Label("Analyse pictures :");
 
-        picturePath = RGUI.Field(picturePath, "Picture path");
-        modelPath = RGUI.Field(modelPath, "Model path");
-        if (GUILayout.Button("Compute")) {
+        //picturePath = RGUI.Field(picturePath, "Picture path");
+        GUILayout.Label(picturePath);
+        if(GUILayout.Button("Load Picture")) {
+            picturePath = EditorUtility.OpenFilePanel("Overwrite with png", "", "png");
+            //LoadLibrary.LoadModel(picture, path);
+        }
 
+        GUILayout.Label(modelPath);
+        if(GUILayout.Button("Load Model")) {
+            modelPath = EditorUtility.OpenFilePanel("Overwrite with csv", "", "csv");
+            //LoadLibrary.LoadModel(model, path);
+        }
+
+        if (GUILayout.Button("Compute")) {
+            // To do
         }
         GUILayout.Label("Result of the analyse (0 is Manga and 1 is BD) => " + result);
 
@@ -60,18 +76,17 @@ public class Interface : MonoBehaviour {
                 model = LoadLibrary.CreateModel((int)modelType, 3, isClassification);
         }
 
-        if (model != IntPtr.Zero) {
-            if (GUILayout.Button("Save Model")) {
-                string path = EditorUtility.SaveFilePanel("Save model as csv", "", "model.csv", "csv");
-                LoadLibrary.SaveModel(model, path);
-            }
+        // if (model != IntPtr.Zero) {
+        //     if (GUILayout.Button("Save Model")) {
+        //         string path = EditorUtility.SaveFilePanel("Save model as csv", "", "model.csv", "csv");
+        //         LoadLibrary.SaveModel(model, path);
+        //     }
 
-            if (GUILayout.Button("Load Model")) {
-                string path = EditorUtility.OpenFilePanel("Overwrite with csv", "", "csv");
-                LoadLibrary.LoadModel(model, path);
-            }
-        }
-
+        //     if (GUILayout.Button("Load Model")) {
+        //         string path = EditorUtility.OpenFilePanel("Overwrite with csv", "", "csv");
+        //         LoadLibrary.LoadModel(model, path);
+        //     }
+        // }
     }
 
     void OnDestroy() {
